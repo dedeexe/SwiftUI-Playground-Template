a39b67fd17801f565b2b79231f155b0911fa777e
Fabricio Dede <dede.exe@gmail.com>
2020-08-04 08:12:18 -0300

    Add First version

---
+++ template/SingleUI View___FileMenu.xctemplate/___FILEBASENAME___.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: A SwiftUI based Playground for presenting user interface
+
+import SwiftUI
+import PlaygroundSupport
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+PlaygroundPage.current.setLiveView(ContentView())
